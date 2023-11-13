7ec1f42565b96ff91e6cd1d0d2b9a7f57998c8af
harsh-agr79 <agrharsh7932@gmail.com>
2023-11-13 23:24:39 +0530

    customer crud except Delete and login done, with dp

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -24,6 +24,7 @@
                         <tr  oncontextmenu="rightmenu({{ $item->id }}); return false;">
                             <td>{{$a = $a + 1}}</td>
                             <td>
+                                
                                 @if ($item->profileimg == NULL)
                                 <img src="{{asset('images/user.png')}}" style="border-radius:50%; height: 60px;" alt="">
                                 @else
