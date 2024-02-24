c658e232950eb14d0b94ab718a586d0f3d67bff6
harsh-agr79 <agrharsh7932@gmail.com>
2024-02-24 11:52:17 +0530

    changes

--- app/Http/Controllers/OrderAdminController.php
+++ app/Http/Controllers/OrderAdminController.php
@@ -213,7 +213,7 @@ class OrderAdminController extends Controller {
     public function addorder( Request $request ) {
         $result[ 'brands' ] = DB::table( 'brands' )->get();
         $result[ 'category' ] = DB::table( 'categories' )->get();
-        $result[ 'data' ] = DB::table( 'products' )->orderBy( 'brand', 'DESC' )->get();
+        $result[ 'data' ] = DB::table( 'products' )->orderBy( 'brand', 'DESC' )->orderBy("category",'DESC')->get();
         return view( 'admin.addorder', $result );
     }
 
