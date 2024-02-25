250ff737d84060056ef8c2b7ea0fd770557d1d5f
harsh-agr79 <agrharsh7932@gmail.com>
2024-02-25 13:20:38 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,9 @@ class HomeController extends Controller
 {
     public function home(){
         $user = DB::table('customers')->where('id', session()->get("USER_ID"))->first();
-        $result['prods'] = DB::table('products')->whereIn('brand_id', explode('|', $user->brands))->where("hide", NULL)->orderBy("brand", 'DESC')->orderBy("category", 'ASC')->get();
+        $result['prods'] = DB::table('products')->whereIn('brand_id', explode('|', $user->brands))->where("hide", NULL)->orderBy("brand", 'DESC')->orderBy("category_id", 'ASC')->get();
         $result['brands'] = DB::table('brands')->whereIn('id', explode('|', $user->brands))->get();
-        $result['category'] = DB::table('categories')->orderBy("category", 'ASC')->get();
+        $result['category'] = DB::table('categories')->get();
         return view('customer/home', $result);
     }
 }
