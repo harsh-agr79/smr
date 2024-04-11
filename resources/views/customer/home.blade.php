06f3fbdae8950c700c5fe0720dca3dd769d7bf14
harsh-agr79 <agrharsh7932@gmail.com>
2024-04-11 16:11:44 +0530

    changes

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -187,7 +187,7 @@
         </div>
     </div>
     <div class="fixed-action-btn">
-        <a class="btn-floating btn-large green accent-4" onclick="getcart()"><i class="material-icons">shopping_cart</i></a>
+        <a class="btn btn-large red" onclick="getcart()"><i class="material-icons">send</i></a>
     </div>
     <div id="cart-modal" class="modal cart-m"
         style="padding: 0 !important; margin: 0 !important; max-width: 95vw !important;">
