0f72cec81a70c2fd754ba1d3b5bf95ca1ad1b38a
harsh-agr79 <agrharsh7932@gmail.com>
2024-04-17 18:50:54 +0530

    changes

--- resources/views/customer/homereal.blade.php
+++ resources/views/customer/homereal.blade.php
@@ -236,23 +236,6 @@
         $('#balpop').remove();
     }
     carousel()
-
-    function carousel() {
-        var caroItem = $('.mp-caro-item');
-        var next = 0;
-        for (let i = 0; i < caroItem.length; i++) {
-            if (!caroItem[i].classList.contains('hide')) {
-                var next = i + 1;
-                if (next > caroItem.length - 1) {
-                    var next = 0;
-                }
-            }
-        }
-        console.log(next)
-        next()
-        setTimeout(carousel, 5000);
-    }
-
     function next() {
         var caroItem = $('.mp-caro-item');
         var next = 0;
@@ -283,4 +266,21 @@
         $('.mp-caro-item').addClass('hide');
         caroItem[next].classList.remove('hide');
     }
+    function carousel() {
+        var caroItem = $('.mp-caro-item');
+        var next = 0;
+        for (let i = 0; i < caroItem.length; i++) {
+            if (!caroItem[i].classList.contains('hide')) {
+                var next = i + 1;
+                if (next > caroItem.length - 1) {
+                    var next = 0;
+                }
+            }
+        }
+        console.log(next)
+        next()
+        setTimeout(carousel, 5000);
+    }
+
+   
 </script>
