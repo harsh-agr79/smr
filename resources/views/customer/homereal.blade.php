1c4f2265e16435b4ca787bcc3f2f1d8b16697f8d
harsh-agr79 <agrharsh7932@gmail.com>
2024-04-17 18:42:18 +0530

    changes

--- resources/views/customer/homereal.blade.php
+++ resources/views/customer/homereal.blade.php
@@ -221,7 +221,7 @@
 </div> --}}
 @endif
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script>
     function closefunc(){
         $('#balpop').remove();
