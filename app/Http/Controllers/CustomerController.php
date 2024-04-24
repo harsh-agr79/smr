88406b0b7266dc400f2ccda8496217864fa38d1b
harsh-agr79 <agrharsh7932@gmail.com>
2024-04-24 22:33:52 +0530

    changes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -63,7 +63,7 @@ class CustomerController extends Controller
     public function editcus_process(Request $request){
         // dd($request->post());
         $request->validate([
-            'userid'=>'required|unique:admins,userid|unique:customers,userid,'.$request->post('id'),           
+            'userid'=>'required|unique:marketers,userid|unique:staffs,userid|unique:admins,userid|unique:customers,userid,'.$request->post('id'),
             'email'=>'required|unique:admins,email|unique:customers,email,'.$request->post('id'), 
             'contact'=>'required|unique:customers,contact,'.$request->post('id'),
             'tax_number'=>'unique:customers,tax_number,'.$request->post('id'),                      
