0af8b9c8c9261ea0618c3b9d051fc0724dc36868
harsh-agr79 <agrharsh7932@gmail.com>
2024-05-05 18:06:23 +0530

    changes

--- resources/views/customer/printcat.blade.php
+++ resources/views/customer/printcat.blade.php
@@ -74,20 +74,19 @@
 
     <script>
          $(document).ready(function() {
-            var invoice = $('#invoice');
+            var inoice = $('#invoice');
             html2pdf(invoice, {
-                margin: [15, 0, 15, 0],
-      filename: `catalog.pdf`,
-      image: { type: 'jpeg', quality: 1 },
-      html2canvas: {
-        dpi: 192,
-        scale:4,
-        letterRendering: true,
-        useCORS: true
-      },
-      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+                filename: `catalog.pdf`,
+                image: { type: 'jpeg', quality: 1 },
+                html2canvas: {
+                  dpi: 192,
+                  scale:4,
+                  letterRendering: true,
+                  useCORS: true
+                },
+                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
             });
-            setTimeout(function() { window.close() }, 15000);
+            // setTimeout(function() { window.close() }, 15000);
         })
     </script>
 </body>
