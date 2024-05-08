333d3532e25f8295dece61402e588a2697c5f916
harsh-agr79 <agrharsh7932@gmail.com>
2024-05-08 14:48:24 +0530

    changes

--- resources/views/customer/printcat.blade.php
+++ resources/views/customer/printcat.blade.php
@@ -7,7 +7,9 @@
     <!--Import materialize.css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+        integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
@@ -19,15 +21,21 @@
             font-size: 11px;
             font-weight: 800;
         }
-        tr{
+
+        tr {
             height: 23% !important;
-            padding-top: 10px !important; 
+            padding-top: 10px !important;
             page-break-inside: avoid;
         }
 
         .cont {
             margin-left: 30vw;
             margin-right: 30vw;
+        }
+
+        .addBreak {
+            break-before: auto;
+            margin-top: 70px;
         }
 
         @media screen and (max-width: 1100px) {
@@ -52,25 +60,38 @@
             <tbody>
                 @foreach ($data as $item)
                     <tr>
-                        <td><span>{{$item->name}} @if ($item->net != NULL)
-                            <span class="red-text">(NET)</span>
-                        @endif</span><br>
-                            @if($item->featured != NULL) <span class="red" style="padding:0 5px; margin: 3px; border-radius: 4px;">NEW!</span>@endif <br> @if($item->offer != NULL) <span class="amber" style="padding: 0 5px; border-radius: 4px; margin: 3px;">{{$item->offer}}</span>@endif <br> @if($item->stock != NULL) <span class="red-text">Out of Stock</span>@endif
+                        <td><span>{{ $item->name }} @if ($item->net != null)
+                                    <span class="red-text">(NET)</span>
+                                @endif
+                            </span><br>
+                            @if ($item->featured != null)
+                                <span class="red" style="padding:0 5px; margin: 3px; border-radius: 4px;">NEW!</span>
+                            @endif <br>
+                            @if ($item->offer != null)
+                                <span class="amber"
+                                    style="padding: 0 5px; border-radius: 4px; margin: 3px;">{{ $item->offer }}</span>
+                            @endif <br>
+                            @if ($item->stock != null)
+                                <span class="red-text">Out of Stock</span>
+                            @endif
                         </td>
-                       
-                        <td>{{$item->category}}</td>
-                        <td>{{$item->brand}}</td>
+
+                        <td>{{ $item->category }}</td>
+                        <td>{{ $item->brand }}</td>
                         @php
                             $a = explode('|', $item->images);
                         @endphp
-                        <td><img src="@if ($item->images != '' || $item->images != null) {{ asset(explode('|', $item->images)[count($a) - 1]) }}@else{{ asset('images/prod.jpg') }} @endif" style="height: 150px;"  alt=""></td>
-                        
-                        <td>{{$item->price}}</td>
-                        <td><div style="white-space: pre-wrap">{{$item->details}}</div></td>
+                        <td><img src="@if ($item->images != '' || $item->images != null) {{ asset(explode('|', $item->images)[count($a) - 1]) }}@else{{ asset('images/prod.jpg') }} @endif"
+                                style="height: 150px;" alt=""></td>
+
+                        <td>{{ $item->price }}</td>
+                        <td>
+                            <div style="white-space: pre-wrap">{{ $item->details }}</div>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
-           </table>
+        </table>
     </div>
 
 
@@ -82,12 +103,47 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
     <script>
-         $(document).ready(function() {
+        $(document).ready(function() {
+            function insertBreaks() {
+
+                //get table rows in html 
+                var rows = document.querySelectorAll('table > tbody > tr');
+                let current_page_height = 0;
+                let max_page_height = 500; //adjust max sizeof page in px 
+                rows.forEach(row => {
+                    var row_height = row.offsetHeight;
+                    current_page_height = current_page_height + row_height
+                    //If the sum of page rows heights are bigger thant my limit, then insert break
+                    if (current_page_height > max_page_height) {
+                        current_page_height = 0;
+                        row.classList.add('addBreak');
+                    }
+                });
+            }
             var inoice = $('#invoice');
             html2pdf(invoice, {
                 filename: 'catalog.pdf',
-                pagebreak: {mode: ['avoid-all', 'css', 'legacy'] },
-                html2canvas:  { scale: 2 , letterRendering: true }
+                margin: [30, 0, 50, 0],
+                image: {
+                    type: 'jpeg',
+                    quality: 0.9
+                },
+                autoPaging: 'text',
+                html2canvas: {
+                    scale: 2,
+                    logging: true,
+                    dpi: 300,
+                    letterRendering: true
+                },
+                jsPDF: {
+                    unit: 'mm',
+                    format: 'a4',
+                    orientation: 'p'
+                },
+                pagebreak: {
+                    avoid: 'tr',
+                    mode: ['css'],
+                }
             });
             // setTimeout(function() { window.close() }, 10000);
         })
@@ -95,4 +151,3 @@
 </body>
 
 </html>
-
