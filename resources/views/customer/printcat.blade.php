8ad5c88979bbf7c87b0fb235face23cf1ca50c1f
harsh-agr79 <agrharsh7932@gmail.com>
2024-05-08 14:55:22 +0530

    changes

--- resources/views/customer/printcat.blade.php
+++ resources/views/customer/printcat.blade.php
@@ -123,7 +123,7 @@
             var inoice = $('#invoice');
             html2pdf(invoice, {
                 filename: 'catalog.pdf',
-                margin: [20, 0, 10, 0],
+                margin: [30, 0, 50, 0],
                 image: {
                     type: 'jpeg',
                     quality: 0.9
