98984275ea7bcc306de5a5d7bdf662156bed077e
harsh-agr79 <agrharsh7932@gmail.com>
2024-05-10 18:26:20 +0530

    changes

--- resources/views/customer/printcat.blade.php
+++ resources/views/customer/printcat.blade.php
@@ -217,13 +217,31 @@
     </script> --}}
 
     <script>
-        $(document).ready(function() {
-            var doc = new jsPDF();
-            doc.fromHTML($('#invoice').html(), 15, 15, {
-                'width': 170,
-            });
-            doc.output('datauri');
-        });
+       window.jsPDF = window.jspdf.jsPDF;
+
+// Convert HTML content to PDF
+function Convert_HTML_To_PDF() {
+    $(document).ready(function(){
+        var doc = new jsPDF();
+	
+    // Source HTMLElement or a string containing HTML.
+    var elementHTML = document.querySelector("#invoice");
+
+    doc.html(elementHTML, {
+        callback: function(doc) {
+            // Save the PDF
+            doc.save('document-html.pdf');
+        },
+        margin: [10, 10, 10, 10],
+        autoPaging: 'text',
+        x: 0,
+        y: 0,
+        width: 190, //target width in the PDF document
+        windowWidth: 675 //window width in CSS pixels
+    });
+    })
+    
+}
     </script>
 </body>
 
