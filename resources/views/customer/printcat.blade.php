ea4ba6d0abf1e1be8ab6a7ba29db6b28a60e8f53
harsh-agr79 <agrharsh7932@gmail.com>
2024-05-10 18:28:18 +0530

    changes

--- resources/views/customer/printcat.blade.php
+++ resources/views/customer/printcat.blade.php
@@ -217,31 +217,35 @@
     </script> --}}
 
     <script>
-       window.jsPDF = window.jspdf.jsPDF;
+        window.jsPDF = window.jspdf.jsPDF;
 
-// Convert HTML content to PDF
-function Convert_HTML_To_PDF() {
-    $(document).ready(function(){
-        var doc = new jsPDF();
-	
-    // Source HTMLElement or a string containing HTML.
-    var elementHTML = document.querySelector("#invoice");
+        $(document).ready(function(){
+            Convert_HTML_To_PDF();
+        })
 
-    doc.html(elementHTML, {
-        callback: function(doc) {
-            // Save the PDF
-            doc.save('document-html.pdf');
-        },
-        margin: [10, 10, 10, 10],
-        autoPaging: 'text',
-        x: 0,
-        y: 0,
-        width: 190, //target width in the PDF document
-        windowWidth: 675 //window width in CSS pixels
-    });
-    })
+        // Convert HTML content to PDF
+        function Convert_HTML_To_PDF() {
+            
+                var doc = new jsPDF();
+
+                // Source HTMLElement or a string containing HTML.
+                var elementHTML = document.querySelector("#invoice");
+
+                doc.html(elementHTML, {
+                    callback: function(doc) {
+                        // Save the PDF
+                        doc.save('document-html.pdf');
+                    },
+                    margin: [10, 10, 10, 10],
+                    autoPaging: 'text',
+                    x: 0,
+                    y: 0,
+                    width: 190, //target width in the PDF document
+                    windowWidth: 675 //window width in CSS pixels
+                });
+            }
+
     
-}
     </script>
 </body>
 
